15c2c2ec90f9869d4c473c0560c7a4d46b68dc2b
ducistheboss <ducistheboss.gmail.com>
2017-03-13 11:29:43 -0400

    add .gitignore and controllers folder

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const routes = require('./controllers/routes.js')
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-	res.render('index.ejs');
-});
+app.get('/', routes.home);
 
 // server start
 app.listen(port);
