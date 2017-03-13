2559218194e136d5f826cf54e1a48518ace466b0
ducistheboss <ducistheboss.gmail.com>
2017-03-13 15:09:48 -0400

    add login, signup, and welcome message for logged in users

--- index.js
+++ index.js
@@ -1,35 +1,157 @@
 'use strict';
 
+// require dependencies
 const express = require('express');
 const session = require('express-session');
-const passport = require('passport');
-const flash = require('connect-flash');
-const configDB = require('./config/database.js');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const mongoose = require('mongoose');
-const app = express();
+const validator = require('validator');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+// connect to database
+const configDB = require('./config/database.js');
+mongoose.connect(configDB.url); // connect to our database
+const Users = require('./models/user.js');
+
+// setup app
+const app = express();
 const host = process.env.IP || '0.0.0.0';
 const port = process.env.PORT || 4000;
+
 const sessionSecret = process.env.SESSION_SECRET || 'e70a1e1ee4b8f662f78';
 
-mongoose.connect(configDB.url); // connect to our database
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 app.use(express.static(__dirname + '/public'));
 
-// setup session with passport
-app.use(session({
-    secret: sessionSecret, // session secret
-    resave: true,
-    saveUninitialized: true
+// Configure our app
+const store = new MongoDBStore({
+  uri: configDB.url,
+  collection: 'sessions',
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true,
 }));
 
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(session({
+  secret: 'thisisasecretsession',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: 'auto',
+  },
+  store,
+}));
 
-// routes ======================================================================
-require('./config/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+function isLoggedIn(req, res, next) {
+	if(res.locals.currentUser) {
+		next();
+	}
+}
+
+app.use(function(req, res, next) {;
+	if(req.session.userId) {
+		Users.findById(req.session.userId, function(err, user) {
+			if(!err) {
+				res.locals.currentUser = user;
+			}
+			next();
+		});
+	}
+	else {
+		next();
+	}
+});
+
+app.get('/', function (req, res) {
+	res.render('index.ejs');
+});
+
+app.get('/signup', function (req, res) {
+	res.render('signup.ejs');
+});
+
+app.get('/login', function (req, res) {
+	res.render('login.ejs');
+});
+
+app.post('/user/register', function (req, res) {
+  if(!validator.isEmail(req.body.email)) {
+		return res.render('index.ejs', { errors: 'Bad email'});
+	}
+
+  if(req.body.email.length < 1 || req.body.email.length > 50) {
+		return res.render('index.ejs', { errors: 'Bad email'});
+	}
+
+  if(req.body.name.length < 1 || req.body.name.length > 50) {
+    return res.render('index.ejs', { errors: 'Bad name'});
+  }
+
+  if(req.body.password.length < 1 || req.body.password.length > 50) {
+    return res.render('index.ejs', { errors: 'Bad password'});
+  }
+
+  var newUser = new Users();
+	newUser.name = req.body.name;
+	newUser.email = req.body.email;
+	newUser.hashed_password = req.body.password;
+
+  newUser.save(function(err, user){
+
+    if(user && !err){
+      req.session.userId = user._id;
+      res.redirect('/');
+      return;
+  	}
+  	var errors = "Error registering you.";
+
+  });
+
+// end of Register post
+
+});
+
+app.post('/user/login', function (req, res) {
+	var user = Users.findOne({email: req.body.email}, function(err, user) {
+		if(err || !user) {
+      console.log('Invalid email address');
+      res.redirect('/login');
+      // res.render('index.ejs', {errors: "Invalid email address"});
+			return;
+		}
+
+    user.comparePassword(req.body.password, function(err, isMatch) {
+			if(err || !isMatch){
+        console.log('Invalid passowrd');
+        res.redirect('/login');
+        //res.render('index.ejs', {errors: 'Invalid password'});
+				return;
+	   		}
+		   	else{
+				req.session.userId = user._id;
+				res.redirect('/');
+				return;
+		   	}
+
+		});
+	});
+
+// end of Log In post
+
+});
+
+app.get('/user/logout', function(req, res){
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
+});
 
 // server start
 const server = app.listen(port, host, function () {
--- models/user.js
+++ models/user.js
@@ -1,25 +1,55 @@
-// load the things we need
-var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-// define the schema for our user model
-var userSchema = mongoose.Schema({
+const SALT_WORK_FACTOR = 10;
 
-        email        : String,
-        password     : String,
-        name         : String
+const Schema = mongoose.Schema;
+
+const stringField = {
+  type: String,
+  minlength: 1,
+  maxlength: 50,
+};
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+    lowercase: true,
+    unique: true,
+  },
+  name: stringField,
+  hashed_password: stringField,
+});
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('hashed_password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.hashed_password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.hashed_password = hash;
+            next();
+        });
+    });
 
 });
 
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.hashed_password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
 };
 
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-};
-
-// create the model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Users', UserSchema);
